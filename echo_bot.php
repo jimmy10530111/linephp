1e3413da8974e7b35b335d0e501c6e457bf5a361
jimmy <55226372+jimmy10530111@users.noreply.github.com>
2019-11-20 00:01:52 +0800

    Add files via upload

--- echo_bot.php
+++ echo_bot.php
@@ -1,27 +1,14 @@
 <?php
 
-session_start();
-//$count = 1;
-$servername = "localhost";
-username = "cat";
-$password = "cat";
-$dbname = "cat";
-//creat connection
-$con=mysqli_connect($servername,$username,$password,$dbname);
-// Check connection
-if (!$con) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-mysqli_set_charset($con,"utf8");
 
 require_once('./LINEBotTiny.php');
+require_once('163.17.27.180/phpsr.php');
 
 $channelAccessToken = '1bb5FOnOqLXnv2W6KeZ+3ms0neF09E8h2KVffW1wjiqSGskGKLQ7/2PDNNBxUWTg6M8UzBtADTqq+hDcec0SbHKRHcVb9Fs8714MJA8MmLWWracX3dnFmJAz5vE7pJErclmgPAE60+M74Cm56+LyEgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '705311288e013e163f3ff55d0e735958';
 
-$sql = "SELECT * FROM gato WHERE 1";
-$result = mysqli_query($con,$sql);
-//$test123='123';
+
+$test123='123';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
